b71bb7d812b83d230462fcfb09b7732495904816
Coach <nuttapong_coach@hotmail.com>
2024-08-08 13:44:00 +0700

    add page

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -15,21 +15,63 @@ const Transactions = () => {
           </tr>
         </thead>
         <tbody>
+          <tr>
             <td>
-              <Image
-                src="/noavatar.png"
-                alt=""
-                width={40}
-                height={40}
-                className={styles.userImage}
-              />
-              John Doe
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
             </td>
             <td>
               <span className={`${styles.status} ${styles.pending}`}>Pending</span>
             </td>
-            <td></td>
-            <td></td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+          <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+          <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>Cancelled</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
         </tbody>
       </table>
     </div>
